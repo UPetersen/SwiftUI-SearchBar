b9fd9eaf009bb5d6b748bed37e4715b01af04cc8
UPetersen <uwe.n.petersen@icloud.com>
2019-10-12 14:07:50 +0200

    Initial Commit

---
+++ SearchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchBar
+//
+//  Created by Uwe Petersen on 12.10.19.
+//  Copyright © 2019 Uwe Petersen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
